f9f04cac9d6bed77fee3417fc04a19b422490290
Lucas Rockwell <lr@lucasrockwell.com>
2024-02-18 15:40:07 -0500

    Updated copyright to be 2024.

--- src/main/groovy/com/canonicalidentity/directoryservice/DirectoryService.groovy
+++ src/main/groovy/com/canonicalidentity/directoryservice/DirectoryService.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Lucas Rockwell
+ * Copyright 2024 Lucas Rockwell
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
